82c4fdcc015cf76e853ee266dba8964b252ad16f
ImadIchiri <88448157+ImadIchiri@users.noreply.github.com>
2022-05-26 19:10:20 +0100

    Add files via upload

---
+++ javaScript/checkWinner.js
@@ -0,0 +1,177 @@
+// function checkWinner() {
+//   //   row 1
+//   if (
+//     squars[0].dataset.character !== "null" &&
+//     squars[1].dataset.character !== "null" &&
+//     squars[2].dataset.character !== "null"
+//   ) {
+//     if (
+//       squars[0].dataset.character === squars[1].dataset.character &&
+//       squars[1].dataset.character === squars[2].dataset.character
+//     ) {
+//       showWinner(0, 2);
+//     }
+//   }
+//   //   row 2
+//   else if (
+//     squars[3].dataset.character !== "null" &&
+//     squars[4].dataset.character !== "null" &&
+//     squars[5].dataset.character !== "null"
+//   ) {
+//     if (
+//       squars[3].dataset.character === squars[4].dataset.character &&
+//       squars[4].dataset.character === squars[5].dataset.character
+//     ) {
+//       showWinner(3, 5);
+//     }
+//   }
+//   //   row 3
+//   else if (
+//     squars[6].dataset.character !== "null" &&
+//     squars[7].dataset.character !== "null" &&
+//     squars[8].dataset.character !== "null"
+//   ) {
+//     if (
+//       squars[6].dataset.character === squars[7].dataset.character &&
+//       squars[7].dataset.character === squars[8].dataset.character
+//     ) {
+//       showWinner(6, 8);
+//     }
+//   }
+//   //   column 1
+//   else if (
+//     squars[0].dataset.character !== "null" &&
+//     squars[3].dataset.character !== "null" &&
+//     squars[6].dataset.character !== "null"
+//   ) {
+//     if (
+//       squars[0].dataset.character === squars[3].dataset.character &&
+//       squars[3].dataset.character === squars[6].dataset.character
+//     ) {
+//       showWinner(0, 6);
+//     }
+//   }
+//   //   column 2
+//   else if (
+//     squars[1].dataset.character !== "null" &&
+//     squars[4].dataset.character !== "null" &&
+//     squars[7].dataset.character !== "null"
+//   ) {
+//     if (
+//       squars[1].dataset.character === squars[4].dataset.character &&
+//       squars[4].dataset.character === squars[7].dataset.character
+//     ) {
+//       showWinner(1, 7);
+//     }
+//   }
+//   //   column 3
+//   else if (
+//     squars[2].dataset.character !== "null" &&
+//     squars[5].dataset.character !== "null" &&
+//     squars[8].dataset.character !== "null"
+//   ) {
+//     if (
+//       squars[2].dataset.character === squars[5].dataset.character &&
+//       squars[5].dataset.character === squars[8].dataset.character
+//     ) {
+//       showWinner(2, 8);
+//     }
+//   }
+//   //   curved line 1
+//   else if (
+//     squars[0].dataset.character !== "null" &&
+//     squars[4].dataset.character !== "null" &&
+//     squars[8].dataset.character !== "null"
+//   ) {
+//     if (
+//       squars[0].dataset.character === squars[4].dataset.character &&
+//       squars[4].dataset.character === squars[8].dataset.character
+//     ) {
+//       console.log("Check Winner ==> 0--8");
+//       showWinner(0, 8);
+//     }
+//   }
+//   //   curved line 2
+//   else if (
+//     squars[2].dataset.character !== "null" &&
+//     squars[4].dataset.character !== "null" &&
+//     squars[6].dataset.character !== "null"
+//   ) {
+//     if (
+//       squars[2].dataset.character === squars[4].dataset.character &&
+//       squars[4].dataset.character === squars[6].dataset.character
+//     ) {
+//       showWinner(2, 6);
+//     }
+//   } else {
+//     showWinner(0, 0);
+//   }
+
+//   //   if (
+//   //     squars[0].dataset.character !== "null" &&
+//   //     squars[1].dataset.character !== "null" &&
+//   //     squars[2].dataset.character !== "null" &&
+//   //     squars[3].dataset.character !== "null" &&
+//   //     squars[4].dataset.character !== "null" &&
+//   //     squars[5].dataset.character !== "null" &&
+//   //     squars[6].dataset.character !== "null" &&
+//   //     squars[7].dataset.character !== "null" &&
+//   //     squars[8].dataset.character !== "null"
+//   //   ) {
+//   //     console.log("Not Empty !!");
+//   //     showWinner(0, 0);
+//   //   }
+// }
+
+function checkWinner() {
+  //   Rows
+  if (checkSquares(0, 1, 2)) {
+    showWinner(0, 2);
+  } else if (checkSquares(3, 4, 5)) {
+    showWinner(2, 5);
+  } else if (checkSquares(6, 7, 8)) {
+    showWinner(6, 8);
+    // Columns
+  } else if (checkSquares(0, 3, 6)) {
+    showWinner(0, 6);
+  } else if (checkSquares(1, 4, 7)) {
+    showWinner(1, 7);
+  } else if (checkSquares(2, 5, 8)) {
+    showWinner(2, 8);
+    // Curved
+  } else if (checkSquares(0, 4, 8)) {
+    showWinner(0, 8);
+  } else if (checkSquares(2, 4, 6)) {
+    showWinner(2, 6);
+  } else {
+    if (
+      squars[0].dataset.character !== "null" &&
+      squars[1].dataset.character !== "null" &&
+      squars[2].dataset.character !== "null" &&
+      squars[3].dataset.character !== "null" &&
+      squars[4].dataset.character !== "null" &&
+      squars[5].dataset.character !== "null" &&
+      squars[6].dataset.character !== "null" &&
+      squars[7].dataset.character !== "null" &&
+      squars[8].dataset.character !== "null"
+    ) {
+      showWinner(0, 0);
+    }
+  }
+}
+
+function checkSquares(squar1, squar2, squar3) {
+  if (
+    squars[squar1].dataset.character !== "null" &&
+    squars[squar2].dataset.character !== "null" &&
+    squars[squar3].dataset.character !== "null"
+  ) {
+    if (
+      squars[squar1].dataset.character === squars[squar2].dataset.character &&
+      squars[squar2].dataset.character === squars[squar3].dataset.character
+    ) {
+      console.log(`Winner ==> ${squar1} -- ${squar3}`);
+      return true;
+    }
+  }
+}
---
+++ javaScript/main.js
@@ -0,0 +1,109 @@
+let squars = document.querySelectorAll(".square");
+let transparent = document.getElementById("transparent");
+let transparentLine = document.getElementById("line");
+let result = document.getElementById("result");
+let gameOver = document.getElementById("game-over");
+let replay = document.getElementById("replay");
+let player_number = Math.floor(Math.random() * 2 + 1);
+
+// When The Game Start
+function whoWillStart() {
+  player_number = Math.floor(Math.random() * 2 + 1);
+  result.textContent = `Player ${player_number} Will Start ( ${
+    player_number === 1 ? "O" : "X"
+  } )`;
+}
+
+// While You Are Playing
+function showPlayerNumber(theSymboal) {
+  result.textContent = `Player ${player_number} ( ${theSymboal} )`;
+}
+
+// Initialise The Player And Symnol
+whoWillStart();
+
+squars.forEach((squar) => {
+  squar.addEventListener("click", (e) => {
+    if (
+      e.currentTarget.firstElementChild === null ||
+      e.currentTarget.firstElementChild === null
+    ) {
+      if (player_number === 1) {
+        addCharacter("o", e.currentTarget);
+        // Check The Winner Before Changing Rules
+        checkWinner();
+        player_number = 2;
+        showPlayerNumber("x");
+      } else {
+        addCharacter("x", e.currentTarget);
+        // Check The Winner Before Changing Rules
+        checkWinner();
+        player_number = 1;
+        showPlayerNumber("o");
+      }
+    }
+  });
+});
+
+function addCharacter(char, position) {
+  let character = document.createElement("i");
+  character.className = `fa-solid fa-${char} player-${char}`;
+  position.append(character);
+  position.dataset.character = char;
+}
+
+function showWinner(start, end) {
+  if (!(start === 0 && end === 0)) {
+    transparent.style.display = "block";
+    removeAllCasses();
+    // Rows
+    if (start === 0 && end === 2) {
+      transparentLine.classList.add("row-1");
+    } else if (start === 3 && end === 5) {
+      transparentLine.classList.add("row-2");
+    } else if (start === 6 && end === 8) {
+      transparentLine.classList.add("row-3");
+    }
+    // Columns
+    else if (start === 0 && end === 6) {
+      transparentLine.classList.add("column-1");
+    } else if (start === 1 && end === 7) {
+      transparentLine.classList.add("column-2");
+    } else if (start === 2 && end === 8) {
+      transparentLine.classList.add("column-3");
+    }
+    // Curved
+    else if (start === 0 && end === 8) {
+      console.log("0--8");
+      transparentLine.classList.add("curved-1");
+    } else if (start === 2 && end === 6) {
+      transparentLine.classList.add("curved-2");
+    }
+  }
+
+  // Show The Game Over Text
+  setTimeout(() => {
+    gameOver.style.display = "flex";
+  }, 500);
+}
+
+replay.addEventListener("click", () => {
+  transparent.style.display = "none";
+  gameOver.style.display = "none";
+  squars.forEach((squar) => {
+    squar.innerHTML = "";
+    squar.dataset.character = "null";
+  });
+  whoWillStart();
+});
+
+function removeAllCasses() {
+  transparentLine.classList.remove("row-1");
+  transparentLine.classList.remove("row-2");
+  transparentLine.classList.remove("row-3");
+  transparentLine.classList.remove("column-1");
+  transparentLine.classList.remove("column-2");
+  transparentLine.classList.remove("column-3");
+  transparentLine.classList.remove("curved-1");
+  transparentLine.classList.remove("curved-2");
+}
